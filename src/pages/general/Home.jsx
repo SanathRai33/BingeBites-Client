573d3aec4fe45104e453b4c77526bbf212deb096
SanathRai <sanathrai03@gmail.com>
2025-09-13 21:34:49 +0530

    Small update

--- src/pages/general/Home.jsx
+++ src/pages/general/Home.jsx
@@ -1,31 +1,31 @@
 import React, { useEffect } from 'react';
 import Reels from '../../component/Reel';
 import '../../styles/Home.css';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // useEffect(() => {
-  //   const getCookie = (name) => {
-  //     const cookies = document.cookie.split('; ');
-  //     for (let cookie of cookies) {
-  //       const [key, value] = cookie.split('=');
-  //       if (key === name) return value;
-  //     }
-  //     return null;
-  //   };
+  useEffect(() => {
+    const getCookie = (name) => {
+      const cookies = document.cookie.split('; ');
+      for (let cookie of cookies) {
+        const [key, value] = cookie.split('=');
+        if (key === name) return value;
+      }
+      return null;
+    };
 
-  //   const token = getCookie('token');
-  //   if (token) {
-  //     localStorage.setItem('token', token);
-  //     console.log('Token saved to localStorage');
-  //   } else {
-  //     console.log('User is not logged in');
-  //     navigate('/user/login');
-  //   }
-  // }, []);
+    const token = getCookie('token');
+    if (token) {
+      localStorage.setItem('token', token);
+      console.log('Token saved to localStorage');
+    } else {
+      console.log('User is not logged in');
+      navigate('/user/login');
+    }
+  }, []);
 
   return (
     <div className="home-container">
