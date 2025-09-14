dd59fe6fde0350c3afbf62fd5445a7721244c0f9
SanathRai <sanathrai03@gmail.com>
2025-09-14 15:41:24 +0530

    loading final update

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -3,11 +3,14 @@ import '../styles/Loading.css';
 const Loading = () => {
 
     return (Array.from({ length: 1 }).map((_, index) => (
-        <div className="video-item skeleton" key={index}>
-            <div className="skeleton-video" />
+        <div className="skeleton" key={index}>
+            {/* <div className="skeleton-video" /> */}
             <div className="skeleton-overlay">
+                <div className="skeleton-info">
+                    <div className="skeleton-avatar"></div>
+                    <div className="skeleton-name" />
+                </div>
                 <div className="skeleton-description" />
-                <div className="skeleton-button" />
             </div>
         </div>
     ))
