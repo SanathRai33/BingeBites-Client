e1b8677224f74d02e5fc73e20e00a82846411e2a
SanathRai <sanathrai03@gmail.com>
2025-09-18 22:35:08 +0530

    Checking user exists

--- src/pages/general/Home.jsx
+++ src/pages/general/Home.jsx
@@ -5,24 +5,27 @@ import '../../styles/Home.css';
 import axios from 'axios';
 
 const Home = () => {
+  
+  const [ profile, setProfile ] = React.useState(null);
+  const navigate = useNavigate();
 
-      const navigate = useNavigate();
-
-    useEffect(() => {
-        axios.get('http://localhost:3000/api/auth/user/profile', {
-            withCredentials: true
-        })
-        .then(response => {
-        })
-        .catch(error => {
-            if (error.response && error.response.status === 401) {
-                navigate('/user/login');
-                console.log("status code",error.response.status)
-            } else {
-                console.error("Error fetching profile:", error);
-            }
-        });
-    }, [navigate]);
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/auth/user/profile`, {
+      withCredentials: true
+    })
+    .then(response => {
+      setProfile(response.data.user);
+      console.log("profile", response.data.user);
+    })
+    .catch(error => {
+      if (error.response && error.response.status === 401) {
+        navigate('/user/login');
+        console.log("status code", error.response.status);
+      } else {
+        console.error("Error fetching profile:", error);
+      }
+    });
+  }, [navigate]);
 
   return (
     <div className="home-container">
