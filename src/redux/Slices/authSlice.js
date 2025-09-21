61378afbe314d7795b413ae2d1eebfeea1379643
SanathRai <sanathrai03@gmail.com>
2025-09-21 23:13:31 +0530

    Debugging redux problem

--- src/redux/Slices/authSlice.js
+++ src/redux/Slices/authSlice.js
@@ -1,26 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  user: null, 
-};
-
 const authSlice = createSlice({
   name: "auth",
-  initialState,
+  initialState: {
+    user: null,
+  },
   reducers: {
     setUser: (state, action) => {
       state.user = {
         id: action.payload.id,
         fullName: action.payload.fullName,
         email: action.payload.email,
-        // phone: action.payload.phone,
-        // address: action.payload.address
       };
     },
     removeUser: (state) => {
       state.user = null;
-    }
-  }
+    },
+  },
 });
 
 export const { setUser, removeUser } = authSlice.actions;
